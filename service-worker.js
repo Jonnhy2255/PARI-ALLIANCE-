213ab19397ebfcd17d32007b6de44b2869b604c6
Jonnhy2255 <billionsjonnhy@gmail.com>
2025-10-01 20:20:41 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,14 +7,33 @@ const urlsToCache = [
   '/icon-512x512.png'
 ];
 
+// Installation + mise en cache
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting(); // le nouveau SW prend la main direct
 });
 
+// Activation + suppression des vieux caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim(); // activer direct sans recharger
+});
+
+// Fetch avec cache-first
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
